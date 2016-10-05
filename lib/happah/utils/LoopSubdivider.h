c4f4c0ece4f7ea8e4b19996cd58e0ea9bc379fc6
Pawel Herman <pawel.herman@kit.edu>
2016-10-05 15:05:50 +0200

    move vertex rule and edge rule into separate class

--- lib/happah/utils/LoopSubdivider.h
+++ lib/happah/utils/LoopSubdivider.h
@@ -16,22 +16,12 @@
 
 namespace happah {
 
-template<class Vertex>
-class LoopSubdivider {
-private:
-    using OutputMesh = TriangleMesh<Vertex, Format::SIMPLE>;
-    using InputMesh = TriangleMesh<Vertex, Format::DIRECTED_EDGE>;
-    using Edge = std::pair<hpuint, hpuint>;
+template<class Iterator>
+class VertexRule {
+     using Vertex = typename Iterator::value_type;
 
-    const InputMesh& m_mesh;
-    const std::vector<Vertex>& m_vertices;
-    const std::vector<hpuint>& m_indices;
-    std::vector<Vertex> m_new_vertices;
-    std::vector<hpuint> m_new_indices;
-    std::unordered_map<Edge, hpuint, boost::hash<Edge>> m_edge_index;
-
-     template<class Iterator>
-     Vertex vertex_rule(const Vertex& center, Iterator begin, Iterator end) const {
+public:
+     Vertex operator()(const Vertex& center, Iterator begin, Iterator end) {
           auto valence = end - begin;
 
           Vertex mean;
@@ -44,12 +34,26 @@ private:
           auto alpha = 3.f / 8.f + 2.f / 8.f * (float)glm::cos(2 * glm::pi<double>() / valence);
           alpha = 5.f / 8.f - alpha * alpha;
 
-          // TODO: generalize for vertices with multiple attributes
           return {alpha * mean.position + (1 - alpha) * center.position};
-     }
+     }     
 
-     // TODO: generalize for vertices with multiple attributes
-     Vertex edge_rule(const Vertex& vertex0, const Vertex& vertex1, const Vertex& vertex2, const Vertex& vertex3) const { return (3.f * (vertex0.position + vertex1.position) + (vertex2.position + vertex3.position)) / 8.f; }
+};//VertexRule
+
+class EdgeRule {
+public:
+     template<class Vertex>
+     Vertex operator()(const Vertex& vertex0, const Vertex& vertex1, const Vertex& vertex2, const Vertex& vertex3) const { return (3.f * (vertex0.position + vertex1.position) + (vertex2.position + vertex3.position)) / 8.f; }
+
+};
+
+template<class Vertex>
+class LoopSubdivider {
+private:
+    using OutputMesh = TriangleMesh<Vertex, Format::SIMPLE>;
+    using InputMesh = TriangleMesh<Vertex, Format::DIRECTED_EDGE>;
+    using Edge = std::pair<hpuint, hpuint>;
+
+    std::unordered_map<Edge, hpuint, boost::hash<Edge>> m_edge_index;//TODO: remove
 
     hpuint edge_index(hpuint v, hpuint w) {
         if (v >= w) std::swap(v, w);
@@ -57,68 +61,78 @@ private:
     }
 
 public:
-     LoopSubdivider(const InputMesh& mesh) : m_mesh(mesh), m_vertices(mesh.getVertices()), m_indices(mesh.getIndices()) {}
+     //TODO: LoopSubdivider();
+     //TODO: template<class VertexRule, class EdgeRule>
+     //LoopSubdivider(VertexRule vertexRule, EdgeRule edgeRule);
 
-     OutputMesh subdivide() {
-        hpuint n = m_vertices.size();
-        hpuint f = m_mesh.getNumberOfTriangles();
-        hpuint e = m_mesh.getNumberOfEdges();
+     OutputMesh subdivide(const InputMesh& mesh) {
+          using VertexRule = VertexRule<typename DeindexedArray<typename InputMesh::Vertices, typename InputMesh::Indices>::const_iterator>;
 
-        m_new_vertices.clear();
-        m_new_indices.clear();
-        m_edge_index.clear();
+          auto& vertices0 = mesh.getVertices();
+          auto& indices0 = mesh.getIndices();
 
-        m_new_vertices.reserve(n + e / 2);
-        m_new_indices.reserve(f * 3 * 4);
-        m_edge_index.reserve(e / 2);
+          hpuint n = vertices0.size();
+          hpuint f = mesh.getNumberOfTriangles();
+          hpuint e = mesh.getNumberOfEdges();
+
+          std::vector<Vertex> vertices1;
+          std::vector<hpuint> indices1;
+
+          vertices1.reserve(n + e / 2);
+          indices1.reserve(f * 3 * 4);
+
+          m_edge_index.clear();//TODO: remove
+          m_edge_index.reserve(e / 2);
 
           // compute new vertex points
           auto v = 0u;
-          for(auto& vertex : m_vertices) {
-               auto temp = m_mesh.getRing(v++);
-               auto ring = deindex(m_vertices, temp);
-               m_new_vertices.emplace_back(vertex_rule(vertex, begin(ring), end(ring)));
+          VertexRule vertexRule;
+          for(auto& vertex : vertices0) {
+               auto temp = mesh.getRing(v++);
+               auto ring = deindex(vertices0, temp);
+               vertices1.emplace_back(vertexRule(vertex, begin(ring), end(ring)));
           }
 
-        // compute new edge points
-        hpuint edge_vertex = m_new_vertices.size();
-        for (hpuint iedge = 0; iedge < e; ++iedge) {
-            auto edge = m_mesh.getEdge(iedge);
-            auto edge_rev = m_mesh.getEdge(edge.opposite);
-            hpuint v, w, x, y;
-            v = edge.vertex;
-            w = edge_rev.vertex;
-            // only process each full edge once
-            if (v < w) {
-                // find neighboring vertices
-                x = m_mesh.getEdge(edge.next).vertex;
-                y = m_mesh.getEdge(m_mesh.getEdge(edge.opposite).next).vertex;
-                m_edge_index.insert(std::make_pair(Edge(v, w), edge_vertex));
-                m_new_vertices.emplace_back(edge_rule(m_vertices[v], m_vertices[w], m_vertices[x], m_vertices[y]));
-                ++edge_vertex;
-            }
-        }
+          // compute new edge points
+          EdgeRule edgeRule;
+          hpuint edge_vertex = vertices1.size();
+          for (hpuint iedge = 0; iedge < e; ++iedge) {
+               auto edge = mesh.getEdge(iedge);
+               auto edge_rev = mesh.getEdge(edge.opposite);
+               hpuint v, w, x, y;
+               v = edge.vertex;
+               w = edge_rev.vertex;
+               // only process each full edge once
+               if (v < w) {
+                    // find neighboring vertices
+                    x = mesh.getEdge(edge.next).vertex;
+                    y = mesh.getEdge(mesh.getEdge(edge.opposite).next).vertex;
+                    m_edge_index.insert(std::make_pair(Edge(v, w), edge_vertex));
+                    vertices1.emplace_back(edgeRule(vertices0[v], vertices0[w], vertices0[x], vertices0[y]));
+                    ++edge_vertex;
+               }
+          }
 
-        // build new index buffer
-        for (hpuint iface = 0; iface < f; ++iface) {
-            hpuint u = m_indices[3 * iface];     // vertex 0
-            hpuint v = m_indices[3 * iface + 1]; // vertex 2
-            hpuint w = m_indices[3 * iface + 2]; // vertex 5
-            hpuint uv = edge_index(u, v);        // vertex 1
-            hpuint vw = edge_index(v, w);        // vertex 4
-            hpuint wu = edge_index(w, u);        // vertex 3
+          // build new index buffer
+          for (hpuint iface = 0; iface < f; ++iface) {
+               auto u = indices0[3 * iface];     // vertex 0
+               auto v = indices0[3 * iface + 1]; // vertex 2
+               auto w = indices0[3 * iface + 2]; // vertex 5
+               auto uv = edge_index(u, v);        // vertex 1
+               auto vw = edge_index(v, w);        // vertex 4
+               auto wu = edge_index(w, u);        // vertex 3
 
-            m_new_indices.insert(m_new_indices.end(), {
-                                     // see TriangleRefinementScheme.cpp
-                                     u,  uv, wu, // 0 1 3
-                                     uv,  v, vw, // 1 2 4
-                                     uv, vw, wu, // 1 4 3
-                                     vw,  w, wu, // 4 5 3
-                                 });
-        }
+               indices1.insert(indices1.end(), {
+                    // see TriangleRefinementScheme.cpp
+                    u,  uv, wu, // 0 1 3
+                    uv,  v, vw, // 1 2 4
+                    uv, vw, wu, // 1 4 3
+                    vw,  w, wu, // 4 5 3
+               });
+          }
 
-        return OutputMesh(m_new_vertices, m_new_indices);
-    }
+          return OutputMesh(vertices1, indices1);
+     }
 
 };//LoopSubdivider
 
