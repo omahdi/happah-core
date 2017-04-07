984a2e5be027e9c36810c070dab7d6485a48ed3b
Pawel Herman <pawel.herman@kit.edu>
2017-04-07 14:39:07 +0200

    refactor writers

---
+++ lib/happah/io/writers/Writer.h
@@ -0,0 +1,36 @@
+// Copyright 2015 - 2017
+//   Pawel Herman - Karlsruhe Institute of Technology - pherman@ira.uka.de
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#include <string>
+
+#include "happah/Happah.h"
+
+namespace happah {
+
+template<hpindex t_format>
+class Writer;
+
+template<class Stream>
+Stream& operator<<(Stream& stream, const hpvec2& v) { 
+     stream << v.x << ' ' << v.y;
+     return stream; 
+}
+
+template<class Stream>
+Stream& operator<<(Stream& stream, const hpvec3& v) { 
+     stream << v.x << ' ' << v.y << ' ' << v.z;
+     return stream; 
+}
+
+template<class Stream>
+Stream& operator<<(Stream& stream, const hpvec4& v) { 
+     stream << v.x << ' ' << v.y << ' ' << v.z << ' ' << v.w;
+     return stream; 
+}
+
+}//namespace happah
+
---
+++ lib/happah/writers.h
@@ -0,0 +1,10 @@
+// Copyright 2015 - 2017
+//   Pawel Herman - Karlsruhe Institute of Technology - pherman@ira.uka.de
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#include "happah/io/writers/WriterHPH.h"
+#include "happah/io/writers/WriterOFF.h"
+
