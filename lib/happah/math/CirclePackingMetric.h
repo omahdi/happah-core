a819919a2adfbe6f3f339236a846c4e836906add
Pawel Herman <pawel.herman@kit.edu>
2017-06-23 18:18:15 +0200

    add missing header

--- lib/happah/math/CirclePackingMetric.h
+++ lib/happah/math/CirclePackingMetric.h
@@ -4,6 +4,7 @@
 // (See accompanying file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #include <cmath>
+#include <glm/gtc/constants.hpp>
 
 #include "happah/geometries/TriangleMesh.h"
 
