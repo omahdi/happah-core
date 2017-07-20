6b49ac524dc754d261dedf22c2b0d6c548763c1d
Pawel Herman <pawel.herman@kit.edu>
2017-07-20 14:08:36 +0200

    introduce test

---
+++ test/happah/geometries/SurfaceSplineBEZ/DeltasEnumerator.cpp
@@ -0,0 +1,14 @@
+// Copyright 2017
+//   Pawel Herman - Karlsruhe Institute of Technology - pherman@ira.uka.de
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <happah/geometries/SurfaceSplineBEZ.h>
+
+int main() {
+     using namespace happah;
+
+     auto e = make_diamonds_enumerator(5, 0, 1);
+     return 0;
+}
+
