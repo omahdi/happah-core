68942542941ce7a3d21682a00e9da748f262e4d3
Pawel Herman <pawel.herman@kit.edu>
2017-08-03 16:20:01 +0200

    fix names

--- test/happah/geometries/Circle/intersect.cpp
+++ test/happah/geometries/Circle/intersect.cpp
@@ -45,47 +45,47 @@ int main() {
      //circles intersect with same x coordinate
      auto circle12 = make_circle({ 2, 2 }, 2);
      auto circle13 = make_circle({ 2, 6 }, 3);
-     auto intersections01 = *intersect(circle12, circle13);
-     auto intersection01 = boost::get<std::tuple<Point2D, Point2D> >(intersections01);
-     assert(glm::length(std::get<0>(intersection01) - Point2D(2 - std::sqrt(135) / 8, 27.0 / 8.0)) < epsilon);
-     assert(glm::length(std::get<1>(intersection01) - Point2D(2 + std::sqrt(135) / 8, 27.0 / 8.0)) < epsilon);
+     auto intersections1 = *intersect(circle12, circle13);
+     auto intersection1 = boost::get<std::tuple<Point2D, Point2D> >(intersections1);
+     assert(glm::length(std::get<0>(intersection1) - Point2D(2 - std::sqrt(135) / 8, 27.0 / 8.0)) < epsilon);
+     assert(glm::length(std::get<1>(intersection1) - Point2D(2 + std::sqrt(135) / 8, 27.0 / 8.0)) < epsilon);
 
      //circles intersect with same y coordinate
      auto circle14 = make_circle({ 3, 1 }, 3);
      auto circle15 = make_circle({ -1, 1 }, 2);
-     auto intersections02 = *intersect(circle14, circle15);
-     auto intersection02 = boost::get<std::tuple<Point2D, Point2D> >(intersections02);
-     assert(glm::length(std::get<0>(intersection02) - Point2D(3.0 / 8.0, 1 - sqrt(135) / 8)) < epsilon);
-     assert(glm::length(std::get<1>(intersection02) - Point2D(3.0 / 8.0, 1 + sqrt(135) / 8)) < epsilon);
+     auto intersections2 = *intersect(circle14, circle15);
+     auto intersection2 = boost::get<std::tuple<Point2D, Point2D> >(intersections2);
+     assert(glm::length(std::get<0>(intersection2) - Point2D(3.0 / 8.0, 1 - std::sqrt(135) / 8)) < epsilon);
+     assert(glm::length(std::get<1>(intersection2) - Point2D(3.0 / 8.0, 1 + std::sqrt(135) / 8)) < epsilon);
 
     //circles intersect
      auto circle16 = make_circle({ 1, 1 }, 1);
      auto circle17 = make_circle({ 3, 0 }, 2);     
-     auto intersections03 = *intersect(circle16, circle17);
-     auto intersection03 = boost::get<std::tuple<Point2D, Point2D> >(intersections03);
-     assert(glm::length(std::get<1>(intersection03) - Point2D(1.0, 0.0)) < epsilon);
-     assert(glm::length(std::get<0>(intersection03) - Point2D(1.8, 16.0/10)) < epsilon);
+     auto intersections3 = *intersect(circle16, circle17);
+     auto intersection3 = boost::get<std::tuple<Point2D, Point2D> >(intersections3);
+     assert(glm::length(std::get<0>(intersection3) - Point2D(1.8, 16.0 / 10)) < epsilon);
+     assert(glm::length(std::get<1>(intersection3) - Point2D(1.0, 0.0)) < epsilon);
 
      //circles are tangent with same x coordinate
      auto circle18 = make_circle({ 3, 2 }, 3);
      auto circle19 = make_circle({ 3, -2 }, 1);
-     auto intersections04 = *intersect(circle18, circle19);
-     auto intersection04 = boost::get<Point2D>(intersections04);
-     assert(glm::length(intersection04 - Point2D(3.0, -1.0)) < epsilon);
+     auto intersections4 = *intersect(circle18, circle19);
+     auto intersection4 = boost::get<Point2D>(intersections4);
+     assert(glm::length(intersection4 - Point2D(3.0, -1.0)) < epsilon);
 
      //circles are tangent with same y coordinate
      auto circle20 = make_circle({ 1, -2 }, 2);
      auto circle21 = make_circle({-4, -2 }, 3);
-     auto intersections05 = *intersect(circle20, circle21);
-     auto intersection05 = boost::get<Point2D>(intersections05);
-     assert(glm::length(intersection05 - Point2D(-1.0, -2.0)) < epsilon);
+     auto intersections5 = *intersect(circle20, circle21);
+     auto intersection5 = boost::get<Point2D>(intersections5);
+     assert(glm::length(intersection5 - Point2D(-1.0, -2.0)) < epsilon);
 
      //circles are tangent
-     auto circle22 = make_circle({ 1, 1 }, sqrt(8));
-     auto circle23 = make_circle({ 4, 4 }, sqrt(2));
-     auto intersections06 = *intersect(circle22, circle23);
-     auto intersection06 = boost::get<Point2D>(intersections06);
-     assert(glm::length(intersection06 - Point2D(3.0, 3.0)) < epsilon);
+     auto circle22 = make_circle({ 1, 1 }, std::sqrt(8));
+     auto circle23 = make_circle({ 4, 4 }, std::sqrt(2));
+     auto intersections6 = *intersect(circle22, circle23);
+     auto intersection6 = boost::get<Point2D>(intersections6);
+     assert(glm::length(intersection6 - Point2D(3.0, 3.0)) < epsilon);
 
      return 0;
 }
