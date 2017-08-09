29ebaab7667f7e4c2735260055705ef9780a42c5
Pawel Herman <pawel.herman@kit.edu>
2017-08-09 15:06:00 +0200

    refactor vertex

--- lib/happah/geometries/Vertex.hpp
+++ lib/happah/geometries/Vertex.hpp
@@ -12,79 +12,74 @@
  * @section DESCRIPTION
  *
  * A vertex is a zero-dimensional geometry with attributes including position, 
- * normal, color, or texture coordinates.  A vertex always has a position or
- * an abscissa and an ordinate.
+ * normal, color, or texture coordinates.  A vertex always has a position.
  */
 
 namespace happah {
 
 template<class Space>
-class Vertex {
+class VertexP {
+     using Point = typename Space::POINT;
+
 public:
      using SPACE = Space;
 
-protected:
-     Vertex() {}
+     Point position{ 0.0 };
 
-};
-
-template<class Space>
-class VertexP : public Vertex<Space> {
-     typedef typename Space::POINT Point;
-
-public:
-     Point position;     
-
-     VertexP() : position(0.0) {}
      VertexP(Point position) : position(std::move(position)) {}
      
-};
-typedef VertexP<Space2D> VertexP2;
-typedef VertexP<Space3D> VertexP3;
-typedef VertexP<Space4D> VertexP4;
+};//VertexP
 
 template<class Space>
-class VertexPC : public VertexP<Space> {
-     typedef typename Space::POINT Point;
+class VertexPC {
+     using Point = typename Space::POINT;
 
 public:
-     hpcolor color {0.0,0.0,0.0,1.0};
-     
-     VertexPC() : VertexP<Space>() {}
-     VertexPC(Point position) : VertexP<Space>(std::move(position)) {}
-     VertexPC(Point position, hpcolor color) : VertexP<Space>(std::move(position)), color(std::move(color)) {}
+     using SPACE = Space;
 
-};
-typedef VertexPC<Space3D> VertexP3C;
+     Point position{ 0.0 };
+     hpcolor color{ 0.0, 0.0, 0.0, 1.0 };
+     
+     VertexPC(Point position, hpcolor color) : position(std::move(position)), color(std::move(color)) {}
+
+};//VertexPC
 
 template<class Space>
-class VertexPN : public Vertex<Space> {
-     typedef typename Space::POINT Point;
-     typedef typename Space::VECTOR Vector;
+class VertexPN {
+     using Point = typename Space::POINT;
+     using Vector = typename Space::VECTOR;
 
 public:   
-     Point position;     
-     Vector normal;
+     using SPACE = Space;
 
-     VertexPN() : position(0.0), normal(0.0) {}
-     VertexPN(Point position) : position(std::move(position)), normal(0.0) {}
+     Point position{ 0.0 };
+     Vector normal{ 0.0 };
+
      VertexPN(Point position, Vector normal) : position(std::move(position)), normal(std::move(normal)) {}
 
-};
-typedef VertexPN<Space3D> VertexP3N;
+};//VertexPN
 
 template<class Space>
-class VertexPNC : public VertexPN<Space> {
-     typedef typename Space::POINT Point;
-     typedef typename Space::VECTOR Vector;
+class VertexPNC {
+     using Point = typename Space::POINT;
+     using Vector = typename Space::VECTOR;
 
 public:
-     hpcolor color;
+     using SPACE = Space;
 
-     VertexPNC() : color(0.0,0.0,0.0,1.0) {}
-     VertexPNC(Point position, Vector normal, hpcolor color) : VertexPN<Space>(std::move(position), std::move(normal)), color(std::move(color)) {}
+     Point position{ 0.0 };
+     Vector normal{ 0.0 };
+     hpcolor color{ 0.0, 0.0, 0.0, 1.0 };
 
-};
+     VertexPNC(Point position, Vector normal, hpcolor color) : position(std::move(position)), normal(std::move(normal)), color(std::move(color)) {}
+
+};//VertexPNC
+
+using VertexP2 = VertexP<Space2D>;
+using VertexP3 = VertexP<Space3D>;
+using VertexP4 = VertexP<Space4D>;
+using VertexP3C = VertexPC<Space3D>;
+using VertexP3N = VertexPN<Space3D>;
 
 template<class Vertex, typename = void>
 struct contains_position : public std::false_type {};
@@ -92,37 +87,42 @@ struct contains_position : public std::false_type {};
 template<class Vertex>
 struct contains_position<Vertex, typename std::enable_if<std::is_base_of<VertexP<typename Vertex::SPACE>, Vertex>::value || std::is_base_of<VertexPN<typename Vertex::SPACE>, Vertex>::value>::type> : public std::true_type {};
 
-template<class V>
-struct contains_color : public std::integral_constant<bool, std::is_base_of<VertexPC<typename V::SPACE>, V>::value || std::is_base_of<VertexPNC<typename V::SPACE>, V>::value> {};
+template<class Vertex>
+struct contains_color : public std::integral_constant<bool, std::is_base_of<VertexPC<typename Vertex::SPACE>, Vertex>::value || std::is_base_of<VertexPNC<typename Vertex::SPACE>, Vertex>::value> {};
 
 template<class Vertex>
 struct contains_normal : public std::integral_constant<bool, std::is_base_of<VertexPN<typename Vertex::SPACE>, Vertex>::value> {};
 
+namespace detail {
+
 template<class Vertex>
-struct do_make_vertex;
+struct make_vertex;
 
 template<>
-struct do_make_vertex<VertexP3> {
+struct make_vertex<VertexP3> {
      template<class Iterator>
      static VertexP3 call(Iterator begin) { return VertexP3(Point3D(begin[0], begin[1], begin[2])); }
 
 };
 
 template<>
-struct do_make_vertex<VertexP3N> {
+struct make_vertex<VertexP3N> {
      template<class Iterator>
      static VertexP3N call(Iterator begin) { return VertexP3N(Point3D(begin[0], begin[1], begin[2]), Vector3D(begin[3], begin[4], begin[5])); }
 
 };
 
 template<>
-struct do_make_vertex<VertexP3C> {
+struct make_vertex<VertexP3C> {
      template<class Iterator>
      static VertexP3C call(Iterator begin) { return VertexP3C(Point3D(begin[0], begin[1], begin[2]), hpcolor(begin[3], begin[4], begin[5], begin[6])); }
 
 };
+
+}//namespace detail
+
 template<class Vertex, class Iterator>
-Vertex make_vertex(Iterator begin) { return do_make_vertex<Vertex>::call(begin); }
+Vertex make_vertex(Iterator begin) { return detail::make_vertex<Vertex>::call(begin); }
 
 }//namespace happah
 
