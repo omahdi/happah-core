2033e79e70d81cdcf6a0ba752b94cbe1c89372db
Pawel Herman <pawel.herman@kit.edu>
2017-08-10 14:39:41 +0200

    fix test includes

--- test/happah/geometry/BezierTriangleMesh/DeltasEnumerator.cpp
+++ test/happah/geometry/BezierTriangleMesh/DeltasEnumerator.cpp
@@ -4,7 +4,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#include <happah/geometry/SurfaceSplineBEZ.hpp>
+#include <happah/geometry/BezierTriangleMesh.hpp>
 
 int main() {
      using namespace happah;
