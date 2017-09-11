0683a55ce9031a9af7d1e18c3c8e64c17b150bcd
Obada Mahdi <omahdi@gmail.com>
2017-09-11 23:40:53 +0200

    Adding test stubs for code in DiskEmbedding.hpp

---
+++ test/happah/geometry/DiskEmbedding/edge-walker.cpp
@@ -0,0 +1,65 @@
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <iomanip>
+#include <happah/Happah.hpp>
+
+// {{{ ---- Logging
+namespace utils {
+void _log_debug(const std::string& msg) {
+     std::cerr << msg << "\n";
+}
+void _log_error(const std::string& msg) {
+// Always flush stderr (via endl)
+     std::cerr << msg << std::endl;
+}
+void _log_output(const std::string& msg) {
+     std::cout << msg << "\n";
+}
+void _log_flush() {
+     std::cout << std::flush;
+}
+}
+// }}} ---- Logging
+// {{{ ---- Namespace imports
+using namespace utils;
+using namespace happah;
+// }}} ---- Namespace imports
+// {{{ ---- Global variables
+constexpr double EPS = 1e-6;
+// }}} ---- Global variables
+// {{{ ---- Test assertions
+unsigned g_testcount = 0;
+unsigned g_testfail = 0;
+#define ASSERT(e)          test_assert(__FILE__, __LINE__, (e), #e);
+#define ASSERT_MSG(e, m)   test_assert(__FILE__, __LINE__, (e), (m));
+#define ASSERT_EQ(a, b)    test_assert(__FILE__, __LINE__, (a == b), #a " != " #b);
+#define SASSERT(e)        test_assert<true>(__FILE__, __LINE__, (e), #e);
+#define SASSERT_MSG(e, m) test_assert<true>(__FILE__, __LINE__, (e), (m));
+#define SASSERT_EQ(a, b)  test_assert<true>(__FILE__, __LINE__, (a == b), #a " != " #b);
+template<bool _strict = false>
+void test_assert(std::string fname, unsigned long lineno, bool expr, std::string msg = "") {
+     using std::to_string;
+     g_testcount++;
+     if (!expr) {
+          g_testfail++;
+          std::string m = fname+':'+to_string(lineno)+
+               std::string(": ")+(msg.size() ? msg : std::string("Failed test assertion"));
+          utils::_log_error(m);
+          if (_strict)
+               throw std::runtime_error(m);
+     }
+}
+// }}} ---- Test assertions
+
+int main() {
+     try {
+          // main tests
+     } catch(const std::exception& err) {
+          utils::_log_error(std::string("Caught exception: ")+std::string(err.what()));
+     }
+     return (g_testfail == 0) ? 0 : 1;
+}
+// vim:ai:bs=2:fo=croq:expandtab:ts=5:sw=5:sbr=+++\ :lbr:bri:wrap
