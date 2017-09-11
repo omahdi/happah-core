1a0d03d88494ddcb7fb075df6063acddb91e1287
Obada Mahdi <omahdi@gmail.com>
2017-09-11 02:08:04 +0200

    Fixing error message in assert function; SASSERT
    - SASSERT...(): strict assertion macros that bail out if assertions fail

--- test/happah/test-template.cpp
+++ test/happah/test-template.cpp
@@ -5,7 +5,6 @@
 #include <iostream>
 #include <iomanip>
 #include <happah/Happah.hpp>
-#include <happah/math/HyperbolicSpace.hpp>
 
 // {{{ ---- Logging
 namespace utils {
@@ -34,20 +33,33 @@ constexpr double EPS = 1e-6;
 // {{{ ---- Test assertions
 unsigned g_testcount = 0;
 unsigned g_testfail = 0;
-#define ASSERT(e)        test_assert(__FILE__, __LINE__, (e), #e);
-#define ASSERT_MSG(e, m) test_assert(__FILE__, __LINE__, (e), (m));
-#define ASSERT_EQ(a, b)  test_assert(__FILE__, __LINE__, (a == b), #a " != " #b);
+#define ASSERT(e)          test_assert(__FILE__, __LINE__, (e), #e);
+#define ASSERT_MSG(e, m)   test_assert(__FILE__, __LINE__, (e), (m));
+#define ASSERT_EQ(a, b)    test_assert(__FILE__, __LINE__, (a == b), #a " != " #b);
+#define SASSERT(e)        test_assert<true>(__FILE__, __LINE__, (e), #e);
+#define SASSERT_MSG(e, m) test_assert<true>(__FILE__, __LINE__, (e), (m));
+#define SASSERT_EQ(a, b)  test_assert<true>(__FILE__, __LINE__, (a == b), #a " != " #b);
+template<bool _strict = false>
 void test_assert(std::string fname, unsigned long lineno, bool expr, std::string msg = "") {
      using std::to_string;
      g_testcount++;
      if (!expr) {
           g_testfail++;
-          utils::_log_error(fname+':'+to_string(lineno)+std::string(": ")+(msg.size() ? msg : std::string("Failed test assertion")));
+          std::string m = fname+':'+to_string(lineno)+
+               std::string(": ")+(msg.size() ? msg : std::string("Failed test assertion"));
+          utils::_log_error(m);
+          if (_strict)
+               throw std::runtime_error(m);
      }
 }
 // }}} ---- Test assertions
 
 int main() {
+     try {
+          // main tests
+     } catch(const std::exception& err) {
+          utils::_log_error(std::string("Caught exception: ")+std::string(err.what()));
+     }
      return (g_testfail == 0) ? 0 : 1;
 }
 // vim:ai:bs=2:fo=croq:expandtab:ts=5:sw=5:sbr=+++\ :lbr:bri:wrap
