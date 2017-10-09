b5126887947e60ce98bb270c228288c5742fcf2d
Pawel Herman <pawel.herman@kit.edu>
2017-10-09 16:25:37 +0200

    refactor make convex polygon

--- lib/happah/math/HyperbolicSpace.hpp
+++ lib/happah/math/HyperbolicSpace.hpp
@@ -1,18 +1,15 @@
-/// \file HyperbolicSpace.hpp
-/// \brief Methods for computations and coordinate transformations in the
-/// various models of hyperbolic space
-///
-/// \author Obada Mahdi <omahdi@gmail.com>
-/// \copyright Copyright 2017 Obada Mahdi <omahdi@gmail.com>
-/// \copyright Distributed under the Boost Software License, Version 1.0.
-/// (See accompanying file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
-///
+// Copyright 2017
+//   Obada Mahdi    -                                       - omahdi@gmail.com
+//   Pawel Herman   - Karlsruhe Institute of Technology     - pherman@ira.uka.de
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
 /// TODO
 /// - direct conversion between U and {P, H} models
 /// - proper conversion of ideal points (if possible)
 /// - directly compute deck transformation in C and U models
-
-#pragma once
 
 #include <array>
 #include <cmath>
@@ -101,7 +98,7 @@ inline std::tuple<hpvec2, double> hyp_geo_C(hpvec2 p, hpvec2 q) {
      const auto det = p.x*q.y - q.x*p.y;
 // special case: p or q are on a diameter, i.e. vectors p, q collinear
      if (glm::abs(det) < EPS)
-          return {hpvec2(0.0, 0.0), 0.0};
+          return std::make_tuple(hpvec2(0.0, 0.0), 0.0);
      const auto lp1 = glm::length2(p) + 1.0, lq1 = glm::length2(q) + 1.0;
      const hpvec2 c {(q.y*lp1 - p.y*lq1) / (2.0*det), (p.x*lq1 - q.x*lp1) / (2.0*det)};
      return std::make_tuple(c, glm::sqrt(glm::length2(c) - 1));
@@ -113,7 +110,7 @@ inline std::tuple<hpvec2, double> hyp_geo_U(hpvec2 p, hpvec2 q) {
      constexpr auto EPS = detail::hyperbolic_EPS();
 // special case: p or q lie on a vertical line
      if (glm::abs(q.x - p.x) < EPS)
-         return {hpvec2((q.x+p.x)/2, 0.0), 0.0};
+         return std::make_tuple(hpvec2((q.x+p.x)/2, 0.0), 0.0);
      const hpvec2 c {(glm::length2(p)-glm::length2(q))/(2*(p.x-q.x)), 0.0};
      return std::make_tuple(c, glm::length(p-c));
 }
@@ -152,118 +149,15 @@ inline double hyp_tesselation_circumradius_P(int p, int q) {
 }
 
 /// Constructs a convex hyperbolic polygon with given interior angles
-/// \p thetas and returns the coordinates of its corner vertices in the
-/// conformal or projective disk models.
+/// \p thetas and returns the Euclidean coordinates of its corner vertices in the
+/// Poincare model.
 ///
-/// The polygon is constructed from quadrilateral pieces that have one corner
-/// at the origin, the opposite one matching a polygon corner with a
-/// prescribed angle, and two right-angled corners on polygon sides. This
-/// method is based on the proof of Theorem 7.16.2 in [1, p. 155f].
+/// Angles \p thetas must be in [0,pi).
+///
+/// See Theorem 7.16.2 in [1, p. 155f] for the mathematics.
 ///
 /// \note [1]: Beardon, "The Geometry of Discrete Groups"
-template<bool _conformal, class F>
-std::vector<hpvec2>
-hyp_polygon_from_angles(const std::vector<F>& thetas, std::integral_constant<bool, _conformal> conformal_mode) { // {{{
-     using std::begin;
-     using std::end;
-// be more tolerant for the sum of angles than for intermediate computations
-     constexpr double eps = 1e-10;
-     constexpr double __attribute__((unused)) sum_eps = 1e-6;
-     const auto sum_thetas = std::accumulate(begin(thetas), end(thetas), 0.0, [](auto s, auto th) { return s+th; });
-     if (sum_thetas - (thetas.size()-2)*M_PI > 0)
-          throw std::runtime_error("hyp_polygon_from_angles_C(): There is no hyperbolic polygon with given angles.");
-
-     double t = 1.0;
-     const auto n = thetas.size();
-// precompute sine and cosine of (theta_i/2) for i=1,...,n
-// used as lookup tables in the evaluation of the function g
-     struct sincos {
-          sincos() = default;
-          sincos(double s, double c) : sin(s), cos(c) { }
-          double sin {0.0}, cos {1.0};
-     };
-     std::vector<sincos> halfthetas;
-     halfthetas.reserve(n);
-     for (auto th : thetas)
-          halfthetas.emplace_back(std::sin(double(th)/2), std::cos(double(th)/2));
-
-// "goal function" g(x): sum of alpha_i for i=1,...,n minus pi, given interior
-// angles (thetas) and hyperbolic length x of the two sides of the
-// quadrilateral patches incident with the origin.
-// Note: In Beardon's proof, g is defined without subtracting pi; we define it
-// in a way such that g(t) = 0 for the desired solution t.
-     auto eval_g = [&] (auto x) {
-          const auto ch = std::cosh(x);
-          return std::accumulate(begin(halfthetas), end(halfthetas), -M_PI,
-               [ch] (auto s, auto ht) { return s + std::asin(ht.cos / ch); });
-     };
-// first derivative of g(x)
-// Recall:
-//   d/dx arcsin(x) = 1/sqrt(1-x^2)
-// Substitute x = x(t) =  cos(theta_k/2) / cosh(t) (chain rule):
-//   d/dx arcsin(x(t)) = (1/sqrt(1-x^2)) * x'(t)
-// with x'(t) = d/dx x(t) = -cos(theta_k/2)*sinh(t)/cosh^2(t)
-//            = -tanh(t)*(cos(theta_k)/cosh(t))
-     auto eval_gprime = [&] (auto x) {
-          const auto ch = std::cosh(x);
-          const auto ch2 = ch*ch;
-          return -std::tanh(x) * std::accumulate(begin(halfthetas), end(halfthetas), 0.0,
-               [ch, ch2] (auto s, auto ht) { return s + (1.0 / std::sqrt(ch2/(ht.cos*ht.cos) - 1.0)); });
-     };
-
-// Newton's method to find root of g, which is a continuous and
-// decreasing function for t > 0.
-     auto g = eval_g(t);
-     unsigned loopmax = 100;
-     while (std::abs(g) > eps && loopmax > 0) {
-          const auto gp = eval_gprime(t);
-          t -= g / gp;
-          g = eval_g(t);
-          loopmax--;
-     }
-     if (loopmax == 0)
-          throw std::runtime_error("hyp_polygon_from_angles_C(): maximum number of iterations reached.");
-     const double ch = std::cosh(t), sh = std::sinh(t);
-     double last_alpha = 0.0, alpha = 0.0, sum_alpha = 0.0;
-     std::vector<hpvec2> vertices;
-     vertices.reserve(n);
-     for (unsigned k = 0; k < n; k++) {
-          const auto& ht_k = halfthetas[k];
-          last_alpha = alpha;
-          alpha = std::asin(ht_k.cos / ch);
-          //const auto cosech_r = ht_k.sin / sh;
-          //const auto rk = 1.0 / (std::sqrt(1 + cosech_r*cosech_r) + cosech_r);
-          const auto sinh_r = sh / ht_k.sin;
-          double rk;
-          if (_conformal)
-               rk = sinh_r / (std::sqrt(sinh_r*sinh_r + 1) + 1);
-          else
-               rk = sinh_r / (std::sqrt(sinh_r*sinh_r + 1));
-          sum_alpha += last_alpha+alpha;
-          vertices.emplace_back(rk*std::cos(sum_alpha), rk*std::sin(sum_alpha));
-     }
-     sum_alpha += alpha;
-// Note: this assertion may fail, even though the computations are
-// theoretically correct, due to rounding errors. This problem is especially
-// prominent when using single-precision computations.
-     //std::cout << "sum_alpha - 2*PI = " << (sum_alpha - 2*M_PI) << "\n";
-     assert(std::abs(sum_alpha - 2.0*M_PI) < sum_eps);
-     return vertices;
-}    // }}}
-
-/// Convenience method that returns the vertices of a polygon with prescribed
-/// interior angles \p thetas in the conformal disk model.
-template<class F>
-auto hyp_polygon_from_angles_C(const std::vector<F>& thetas) {
-     return hyp_polygon_from_angles(thetas, std::true_type());
-}
-
-/// Convenience method that returns the vertices of a polygon with prescribed
-/// interior angles \p thetas in the projective disk model.
-template<class F>
-auto hyp_polygon_from_angles_P(const std::vector<F>& thetas) {
-     return hyp_polygon_from_angles(thetas, std::false_type());
-}
+std::vector<Point2D> make_convex_polygon(const std::vector<hpreal>& angles, hpreal epislon = EPSILON);
 
 /// Computes a projective frame uniquely determining the line through \p p and
 /// \p q as well as the point \p p itself, using a construction based on three
@@ -346,4 +240,6 @@ Matrix3 hyp_decktrans_P(hpvec2 _p1, hpvec2 _q1, hpvec2 _p2, hpvec2 _q2) { // {{{
      const auto result {(S2*ldiag)*inv_S1};
      return result;
 }    // }}}
+
 }    // namespace happah
+
