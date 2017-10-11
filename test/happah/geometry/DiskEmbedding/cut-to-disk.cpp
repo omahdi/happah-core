7feb8f41a931178b125cb792619359745482b438
Obada Mahdi <omahdi@gmail.com>
2017-10-11 22:36:42 +0200

    Generating new minitorus

--- test/happah/geometry/DiskEmbedding/cut-to-disk.cpp
+++ test/happah/geometry/DiskEmbedding/cut-to-disk.cpp
@@ -55,10 +55,57 @@ void test_assert(std::string fname, unsigned long lineno, bool expr, std::string
 }
 // }}} ---- Test assertions
 
+template<class _Vertex>
+auto
+build_minitorus(unsigned _segments, double _height = 1.0, double _r_outer = -1, double _r_inner = -1) {
+     using namespace happah;
+     using std::sin;
+     using std::cos;
+     using Point = decltype(std::declval<_Vertex>().position);
+     if (_height <= 0)
+          throw std::runtime_error("build_minitorus(): height must be positive");
+     const double height  = _height;
+     // use some sensible defaults
+     const double r_outer = _r_outer > 0 ? _r_outer : 2.0*_height;
+     const double r_inner = _r_inner > 0 ? _r_inner : _r_outer / std::sqrt(2.0);
+     if (r_inner >= r_outer)
+          throw std::runtime_error("build_minitorus(): inner radius must be smaller than outer radius");
+     const double phi = 2*M_PI / _segments;
+     std::vector<_Vertex> vertices;
+     std::vector<hpindex> indices;
+     if (_segments < 3)
+          throw std::runtime_error("build_minitorus(): need at least three segments");
+     vertices.reserve(3*_segments);
+     indices.reserve(6*_segments);
+     for (hpindex s = 0; s < _segments; s++) {
+          vertices.emplace_back(Point{r_outer*cos(s*phi), r_outer*sin(s*phi), -height/2.0});
+          vertices.emplace_back(Point{r_outer*cos(s*phi), r_outer*sin(s*phi),  height/2.0});
+          vertices.emplace_back(Point{r_inner*cos(s*phi), r_inner*sin(s*phi),  0.0});
+     }
+     const auto num_verts = vertices.size();
+     for (hpindex s = 0; s < _segments; s++) {
+          const auto offset = 3*s;
+          auto push_triplet = [&indices, num_verts, offset](auto u, auto v, auto w) {
+               indices.emplace_back((offset + u) % num_verts);
+               indices.emplace_back((offset + v) % num_verts);
+               indices.emplace_back((offset + w) % num_verts);
+          };
+          push_triplet(0, 4, 3);
+          push_triplet(0, 1, 4);
+          push_triplet(1, 5, 4);
+          push_triplet(1, 2, 5);
+          push_triplet(2, 3, 5);
+          push_triplet(2, 0, 3);
+     }
+     return happah::make_triangle_mesh<_Vertex>(std::move(vertices), std::move(indices));
+}
+
 int main() {
      try {
-          auto raw_mesh {format::off::read("minitorus.off")};
-          auto mesh {make_triangle_mesh<VertexP3>(raw_mesh)};
+          //auto raw_mesh {format::off::read("minitorus.off")};
+          //auto mesh {make_triangle_mesh<VertexP3>(raw_mesh)};
+          auto mesh {build_minitorus<VertexP3>(4, 0.5, 1.0, 0.4)};
+          format::off::write(mesh, "mt-new.off");
           const auto& verts {mesh.getVertices()};
           bool table_format = false;
           auto print_coords = [&table_format, &verts] (auto v0, auto v1, auto v2) {
