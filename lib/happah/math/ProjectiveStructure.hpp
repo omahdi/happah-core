86b08a33f5a14611a2d4f3940905d7d59143ae1b
Pawel Herman <pawel.herman@kit.edu>
2017-11-13 15:42:20 +0100

    refactor make_projective_structure from triangle mesh

--- lib/happah/math/ProjectiveStructure.hpp
+++ lib/happah/math/ProjectiveStructure.hpp
@@ -5,7 +5,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-// 2017.10 - Louis Lutzweiler - Added make_projective_structure.
+// 2017.10 - Louis Lutzweiler - Added make_projective_structure from a genus zero triangle mesh.
 
 #pragma once
 
