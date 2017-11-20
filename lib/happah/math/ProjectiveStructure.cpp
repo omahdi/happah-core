ad0f16b6299303807902b47d3acb43b85c580d6a
Pawel Herman <pawel.herman@kit.edu>
2017-11-20 15:31:58 +0100

    refactor weight calculation in make_sun

--- lib/happah/math/ProjectiveStructure.cpp
+++ lib/happah/math/ProjectiveStructure.cpp
@@ -144,48 +144,20 @@ std::vector<Point3D> make_sun(const Indices& valences, const Indices& pairings) 
      auto sun = std::vector<Point3D>();
      auto polygon = make_convex_polygon(valences);
      auto d = std::asinh(std::sinh(hpreal(2) * std::atanh(glm::length(polygon[0]))) * std::sin(glm::pi<hpreal>() / hpreal(valences[0])));
-     auto r = std::tanh(d);
+     auto r0 = std::tanh(d / hpreal(2));
+     auto r1 = std::tanh(d);
      auto c = std::cosh(d);
-     auto factor = (hpreal(2) * r) / (hpreal(1) + r * r);
+     auto l0 = (hpreal(2) * r0) / (hpreal(1) + r0 * r0);
+     auto l1 = (hpreal(2) * r1) / (hpreal(1) + r1 * r1);
+     auto w = l0 / (l1 - l0);
      auto sum = hpreal(0);
-     auto cache = std::vector<hpreal>();
-
-     auto lambda = [&](auto& x, auto& y, auto& z) {
-          auto matrix = glm::inverse(hpmat3x3(x, y, z));
-          cache.push_back(matrix[2][0]);
-          cache.push_back(matrix[2][1]);
-          z *= -matrix[2][2];
-          x.z = std::numeric_limits<hpreal>::max();
-     };
 
      sun.reserve(n << 1);
-     cache.reserve(n << 1);
+     l1 *= w;
      for(auto& point : polygon) sun.emplace_back((hpreal(2) / (hpreal(1) + glm::length2(point))) * point, hpreal(1));
      for(auto& valence : valences) {
           sum += hpreal(2) * std::asin(std::cos(glm::pi<hpreal>() / valence) / c);
-          sun.emplace_back(factor * std::cos(sum), factor * std::sin(sum), hpreal(1));
-     }
-     for(auto i = std::begin(sun), j = i + n, end = j - 1; i != end; ++i, ++j) lambda(*i, *(i + 1), *j);
-     sun.front().z = hpreal(1);
-     lambda(sun[n - 1], sun.front(), sun.back());
-     sun.front().z = std::numeric_limits<hpreal>::max();
-
-     for(auto i = hpindex(0); i != n; ++i) {
-          if(sun[i].z != std::numeric_limits<hpreal>::max()) continue;
-          auto begin = i;
-          auto weight = hpreal(1);
-          do {
-               weight /= cache[i << 1];
-               i = pairings[i];
-               weight *= cache[(i << 1) + 1];
-               assert(weight > 0);
-               if(++i == n) i = hpindex(0);
-               auto& point = sun[i];
-               assert(point.z == std::numeric_limits<hpreal>::max());
-               point.x *= weight;
-               point.y *= weight;
-               point.z = weight;
-          } while(i != begin);
+          sun.emplace_back(l1 * std::cos(sum), l1 * std::sin(sum), w);
      }
 
      return sun;
